debdc7456b41c262a48975743596cc18e4bd456a
Stuart Clark <stu@rtclark.net>
2021-08-10 13:35:33 +0000

    feat: add initial prototype.

---
+++ nuxt/components/druxt/entity/node/card/Default.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="border-2 rounded-xl p-2 h-35 w-25 text-center">
+    <h2>{{ model.attributes.title }}</h2>
+  </div>
+</template>
+
+<script>
+import { DruxtEntityMixin } from 'druxt-entity'
+
+export default {
+  mixins: [DruxtEntityMixin],
+}
+</script>
--- nuxt/pages/index.vue
+++ nuxt/pages/index.vue
@@ -1,7 +1,39 @@
 <template>
-  <Tutorial/>
+  <div>
+    <Draggable v-model="cards" class="flex space-x-2">
+      <DruxtEntity
+        v-for="card of hand"
+        :key="card.id"
+        type="node--card"
+        :uuid="card.id"
+      />
+    </Draggable>
+  </div>
 </template>
 
 <script>
-export default {}
+import Deck from 'card-deck'
+import Draggable from 'vuedraggable'
+
+export default {
+  components: { Draggable },
+
+  data: () => ({
+    cards: [],
+    deck: new Deck(),
+    hand: new Deck(),
+  }),
+
+  async fetch() {
+    // Get all cards from Drupal.
+    const collections = await this.$druxt.getCollectionAll('node--card')
+    this.cards = collections.map(({ data }) => data).flat()
+
+    // Add cards to the deck.
+    this.deck.cards(this.cards)
+    this.deck.shuffle()
+
+    this.hand = this.deck.draw(3)
+  }
+}
 </script>
